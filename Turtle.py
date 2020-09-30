852f33329f0d4c78beceb00e4e6a370e28207f06
ankita mandal <56070901+ankitamandal329@users.noreply.github.com>
2020-10-01 00:44:55 +0530

    Update Turtle.py

--- Turtle.py
+++ Turtle.py
@@ -1,8 +1,8 @@
 import turtle
 pen = turtle.Turtle()
 
-pen.pendown() #this line puts the pen on the paper
-pen.forward(100) #now move the pen forward 100 steps!
+pen.pendown() 
+pen.forward(10)
 pen.right(90)
 pen.forward(10)
 pen.right(90)
