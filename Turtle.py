2751b0ce6a1bc61580d9d86f1cc599993fd7e243
aamirkhn035 <57729064+aamirkhn035@users.noreply.github.com>
2020-10-01 00:33:12 +0530

    Create Turtle.py

---
+++ Turtle.py
@@ -0,0 +1,11 @@
+import turtle
+pen = turtle.Turtle()
+
+pen.pendown() #this line puts the pen on the paper
+pen.forward(100) #now move the pen forward 100 steps!
+pen.right(90)
+pen.forward(100)
+pen.right(90)
+pen.forward(100)
+pen.right(90)
+pen.forward(100)
